864ffdac28c6eda8666d03919d73efc29d4fe05c
Meritxell Jordan <meritxell98@hotmail.com>
2026-07-02 15:35:06 +0200

    function divide with error handeling

--- script.js
+++ script.js
@@ -7,4 +7,10 @@ const subtract = (a, b) => a - b;
 
 const multiply = (a, b) => a * b;
 
-const divide = (a, b) => a / b;
+function divide(a, b) {
+  if (b === 0) {
+    return 'Error';
+  }
+  return a / b;
+}
+
