19ae7cb9bf87cabb17fef96b199932e474b803fa
Meritxell Jordan <meritxell98@hotmail.com>
2026-07-02 15:33:40 +0200

    math operations

--- script.js
+++ script.js
@@ -1,3 +1,10 @@
 const display =document.getElementById('display');
 const buttons = document.getElementById('buttons');
 
+const add = (a, b) => a + b;
+
+const subtract = (a, b) => a - b;
+
+const multiply = (a, b) => a * b;
+
+const divide = (a, b) => a / b;
